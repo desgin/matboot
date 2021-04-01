ef6ef887afac5013d5fb6e97c2ba1b1c292ac87e
Devi <devi.frontenddev@gmail.com>
2021-03-31 23:13:30 -0500

    adding the register form

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -4,13 +4,14 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { DataTableComponent } from './data-table/data-table.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
 
 const storeRoutes: Routes = [{
   path: 'dataTable', component: DataTableComponent
 }];
 
 @NgModule({
-  declarations: [DashboardComponent, DataTableComponent],
+  declarations: [DashboardComponent, DataTableComponent, RegisterFormComponent],
   imports: [
     CommonModule,
     MatTabsModule, 
